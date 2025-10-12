582195f97baca2d5e1d303adedae3de8a811239e
Azuraken05 <kaichouazura@gmail.com>
2025-10-12 13:57:44 +0800

    newdashboard

---
+++ frmNewDashboard.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WONG_BANKING
+{
+    partial class frmNewDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnWithdraw = new System.Windows.Forms.Button();
+            this.btnDeposit = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.picCarousel = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.picProfile = new WONG_BANKING.CircularPictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCarousel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picProfile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.lblWelcome);
+            this.panel1.Controls.Add(this.picProfile);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.btnDeposit);
+            this.panel1.Controls.Add(this.btnWithdraw);
+            this.panel1.Controls.Add(this.btnRegister);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(222, 487);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(25, 139);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(162, 35);
+            this.btnRegister.TabIndex = 0;
+            this.btnRegister.Text = "REGISTRATION";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnWithdraw
+            // 
+            this.btnWithdraw.Location = new System.Drawing.Point(25, 194);
+            this.btnWithdraw.Name = "btnWithdraw";
+            this.btnWithdraw.Size = new System.Drawing.Size(162, 35);
+            this.btnWithdraw.TabIndex = 1;
+            this.btnWithdraw.Text = "WITHDRAW";
+            this.btnWithdraw.UseVisualStyleBackColor = true;
+            this.btnWithdraw.Click += new System.EventHandler(this.btnWithdraw_Click);
+            // 
+            // btnDeposit
+            // 
+            this.btnDeposit.Location = new System.Drawing.Point(25, 248);
+            this.btnDeposit.Name = "btnDeposit";
+            this.btnDeposit.Size = new System.Drawing.Size(162, 35);
+            this.btnDeposit.TabIndex = 2;
+            this.btnDeposit.Text = "DEPOSIT";
+            this.btnDeposit.UseVisualStyleBackColor = true;
+            this.btnDeposit.Click += new System.EventHandler(this.btnDeposit_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(25, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "CHECK BALANCE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(25, 359);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(162, 35);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "DISPLAY";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(25, 412);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(162, 35);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "LOG OUT";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // picCarousel
+            // 
+            this.picCarousel.Location = new System.Drawing.Point(348, 127);
+            this.picCarousel.Name = "picCarousel";
+            this.picCarousel.Size = new System.Drawing.Size(431, 262);
+            this.picCarousel.TabIndex = 1;
+            this.picCarousel.TabStop = false;
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(35, 114);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(33, 16);
+            this.lblWelcome.TabIndex = 7;
+            this.lblWelcome.Text = "N/A";
+            this.lblWelcome.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // picProfile
+            // 
+            this.picProfile.Location = new System.Drawing.Point(52, 12);
+            this.picProfile.Name = "picProfile";
+            this.picProfile.Size = new System.Drawing.Size(101, 89);
+            this.picProfile.TabIndex = 6;
+            this.picProfile.TabStop = false;
+            // 
+            // frmNewDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WONG_BANKING.Properties.Resources.bgDashboard;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(845, 487);
+            this.ControlBox = false;
+            this.Controls.Add(this.picCarousel);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmNewDashboard";
+            this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.frmNewDashboard_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCarousel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picProfile)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnDeposit;
+        private System.Windows.Forms.Button btnWithdraw;
+        private System.Windows.Forms.PictureBox picCarousel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblWelcome;
+        private CircularPictureBox picProfile;
+    }
+}
